a84ba4d8e81ce0abd39f7be27afab4e6c714ae97
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-03-06 22:15:28 +0530

    getInitialCash changed to getCashInHand,register expense method added and tested

--- src/com/moneyManager/MoneyManager.java
+++ src/com/moneyManager/MoneyManager.java
@@ -1,14 +1,22 @@
 package com.moneyManager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MoneyManager{
 
-    private int initialCash = 0;
-
+    private double initialCash = 0;
+    public Map<String, Double> expenseJournal = new HashMap<String, Double>();
     public MoneyManager(int initialCash) {
         this.initialCash = initialCash;
     }
 
-    public int getStartingCash() {
+    public double getCashInHand() {
         return this.initialCash;
+    }
+
+    public void registerExpensen(String expenseAccount, double amount) {
+        this.expenseJournal.put(expenseAccount,amount);
+        this.initialCash-=amount;
     }
 }
--- test/com/moneyManager/MoneyManagerTest.java
+++ test/com/moneyManager/MoneyManagerTest.java
@@ -14,8 +14,15 @@ public class MoneyManagerTest {
         assertTrue(true);
     }
     @Test
-    public void MoneyManager_holds_starting_money_for_each_day(){
+    public void MoneyManager_holds_Cash_in_hand_at_any_point_of_day_for_each_day(){
         MoneyManager mgr = new MoneyManager(10000);
-        assertEquals(mgr.getStartingCash(), 10000);
+        assertEquals(mgr.getCashInHand(), 10000,1.0);
     }
+    @Test
+    public void MoneyManager_records_the_expenses_generated_by_the_user_and_calculates_cash_in_hand(){
+        MoneyManager mgr = new MoneyManager(1000);
+        mgr.registerExpensen("Shopping",150);
+        assertEquals(mgr.getCashInHand(),850,1.0);
+    }
+
 }
